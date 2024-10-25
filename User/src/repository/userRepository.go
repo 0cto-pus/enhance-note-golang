4048d06f1221c50a1785627494d14f373dc3860c
Octopus <octopus@octopusdev.com>
2024-10-25 21:11:09 +0300

    add repository outlines

--- User/src/repository/userRepository.go
+++ User/src/repository/userRepository.go
@@ -1,1 +1,30 @@
 package repository
+
+import (
+	"enhanced-notes/src/domain"
+
+	"gorm.io/gorm"
+)
+
+type IUserRepository interface {
+	CreateUser(user domain.User)
+	FindUser(userId uint64) (domain.User, error)
+}
+
+type UserRepository struct{
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) IUserRepository{
+	return &UserRepository{
+		db:db,
+	}
+}
+
+func (userRepository *UserRepository) CreateUser(user domain.User){
+	
+}
+
+func (userRepository *UserRepository) FindUser(userId uint64) (domain.User,error){
+	return domain.User{}, nil
+}
