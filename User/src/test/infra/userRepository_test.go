6c122a0337094888137dec87c25925cc1d90b3a3
Octopus <octopus@octopusdev.com>
2024-10-26 18:43:09 +0300

    Create User Test & Repository

--- User/src/test/infra/userRepository_test.go
+++ User/src/test/infra/userRepository_test.go
@@ -1,13 +1,62 @@
 package infra
 
 import (
+	"context"
+	"enhanced-notes/src/domain"
+	"enhanced-notes/src/repository"
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+var userRepository repository.IUserRepository
+var db *gorm.DB
+var ctx context.Context
+
 func TestMain(m *testing.M){
-	/*  expectedValue := domain.User{
+
+	var dsn string = "host=127.0.0.1 user=postgres password=root dbname=postgres port=6432 sslmode=disable"
+	gormOpen, err := gorm.Open(postgres.Open(dsn), &gorm.Config{});
+
+	db = gormOpen;
+	if err != nil {
+		panic( err)
+	}
+
+
+	userRepository = repository.NewUserRepository(db)
+	exitCode := m.Run()
+	os.Exit(exitCode)
+
+}
+func setup(ctx context.Context, db *gorm.DB) {
+	TestDataInitialize(ctx, db)
+}
+func clear(ctx context.Context, db *gorm.DB) {
+	TruncateTestData(ctx, db)
+}
+
+func TestCreateUser(t *testing.T){
+
+	 newUser := domain.User{
 		Email: "test@test.com",
 		Password: "hash-pass-mock",
-	 } */
+	 }
+
+	 t.Run("CreateUser", func(t *testing.T) {
+		userRepository.CreateUser(newUser)
+		addedUser, _ := userRepository.FindUserById(1)
+		
+		assert.Equal(t, domain.User{
+			ID: 1,
+			Email: "test@test.com",
+			Password: "hash-pass-mock",
+			CreatedAt: addedUser.CreatedAt,
+			UpdatedAt: addedUser.UpdatedAt,}, addedUser)
+	})
+	clear(ctx, db)
 
 }
